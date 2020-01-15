5ec677cb90ccfabd62d97c75b469687a20fca422
Daniel <mossur15@gmail.com>
2020-01-15 17:14:27 +0200

    replaced fabric with class

--- app/src/main/java/com/example/myapplication/presentation/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/fragments/NewsFragment.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication.presentation.fragments
 
 import androidx.lifecycle.Observer
-import com.demoss.paginatorrenderkit.view.adapter.delegate.PaginatorAdapterDelegateFabric
+import com.demoss.paginatorrenderkit.view.adapter.delegate.PaginatorAdapterDelegate
 import com.demoss.paginatorrenderkit.view.delegate.PaginatorViewDelegate
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
@@ -22,7 +22,7 @@ class NewsFragment : BaseFragment<NewsViewModel>() {
             viewModel::refresh,
             viewModel::loadNextPage,
             pvArticles,
-            PaginatorAdapterDelegateFabric.create(R.layout.item_article) { ArticleVH(it) }
+            PaginatorAdapterDelegate(R.layout.item_article) { ArticleVH(it) }
         )
     }
 
--- paginatorrenderkit/src/main/java/com/demoss/paginatorrenderkit/view/adapter/delegate/AdapterDelegateDsl.kt
+++ paginatorrenderkit/src/main/java/com/demoss/paginatorrenderkit/view/adapter/delegate/AdapterDelegateDsl.kt
@@ -1,5 +1,6 @@
 package com.demoss.paginatorrenderkit.view.adapter.delegate
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
@@ -9,28 +10,53 @@ import com.demoss.paginatorrenderkit.view.inflate
 import com.demoss.paginatorrenderkit.view.model.PaginatorItem
 import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
 
-object PaginatorAdapterDelegateFabric {
+//object PaginatorAdapterDelegateFabric {
+//
+//    inline fun <reified T : Any> create(
+//        @LayoutRes layout: Int,
+//        crossinline viewHolderFabric: (View) -> AbsPaginatorVH<in T>
+//    ) = object : AdapterDelegate<MutableList<PaginatorItem<*>>>() {
+//
+//        override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder =
+//            viewHolderFabric(parent.inflate(layout))
+//
+//        override fun isForViewType(items: MutableList<PaginatorItem<*>>, position: Int): Boolean =
+//            items[position].isForViewType(T::class.java)
+//
+//        override fun onBindViewHolder(
+//            items: MutableList<PaginatorItem<*>>,
+//            position: Int,
+//            holder: RecyclerView.ViewHolder,
+//            payloads: MutableList<Any>
+//        ) {
+//            @Suppress("UNCHECKED_CAST")
+//            holder as AbsPaginatorVH<in T>
+//            holder.bindData(items[position].data as T)
+//        }
+//    }
+//}
 
-    inline fun <reified T : Any> create(
-        @LayoutRes layout: Int,
-        crossinline viewHolderFabric: (View) -> AbsPaginatorVH<in T>
-    ) = object : AdapterDelegate<MutableList<PaginatorItem<*>>>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder =
-            viewHolderFabric(parent.inflate(layout))
+open class PaginatorAdapterDelegate<T : Any> constructor(
+    @LayoutRes val layout: Int,
+    private val viewHolderFabric: (View) -> AbsPaginatorVH<in T>
+) : AdapterDelegate<MutableList<PaginatorItem<*>>>() {
 
-        override fun isForViewType(items: MutableList<PaginatorItem<*>>, position: Int): Boolean =
-            items[position].isForViewType(T::class.java)
+    final override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder =
+        viewHolderFabric(parent.inflate(layout))
 
-        override fun onBindViewHolder(
-            items: MutableList<PaginatorItem<*>>,
-            position: Int,
-            holder: RecyclerView.ViewHolder,
-            payloads: MutableList<Any>
-        ) {
-            @Suppress("UNCHECKED_CAST")
-            holder as AbsPaginatorVH<in T>
-            holder.bindData(items[position].data as T)
-        }
+    final override fun isForViewType(items: MutableList<PaginatorItem<*>>, position: Int): Boolean =
+        items[position].isForViewType(T::class.java)
+
+    override fun onBindViewHolder(
+        items: MutableList<PaginatorItem<*>>,
+        position: Int,
+        holder: RecyclerView.ViewHolder,
+        payloads: MutableList<Any>
+    ) {
+        @Suppress("UNCHECKED_CAST")
+        holder as AbsPaginatorVH<in T>
+        @Suppress("UNCHECKED_CAST")
+        holder.bindData(items[position].data as T)
     }
 }
